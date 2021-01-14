72e4bb6dfbd95830974abe1341c1b10143e0916e
Ckyiu <UnsignedArduino@outlook.com>
2021-01-14 12:19:06 -0500

    Change width every 4 levels

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -13,6 +13,8 @@ namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const tile5 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
     export const transparency16 = image.ofBuffer(hex``);
 
 }
