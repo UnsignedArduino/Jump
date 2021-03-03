cc8b32c14beb2ca08eb44ca6fe25f659c07fa637
Ckyiu <UnsignedArduino@outlook.com>
2021-03-03 13:05:54 -0500

    Clean look of blocks

---
+++ images.g.ts
@@ -0,0 +1,111 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":return img`
+7 7 7 7 
+7 7 7 7 
+7 7 7 7 
+7 7 7 7 
+`;
+            case "image2":return img`
+. 8 8 8 8 8 . . 
+8 8 8 8 8 8 8 . 
+. 6 f 6 f 6 . . 
+. 6 6 6 6 6 . . 
+. 6 6 f 6 6 . . 
+. 6 6 6 6 6 . . 
+. 6 . . . 6 . . 
+. 6 . . . 6 . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "anim1":return [img`
+. 8 8 8 8 8 . . 
+8 8 8 8 8 8 8 . 
+. f 6 f 6 6 . . 
+. 6 6 6 6 6 . . 
+. 6 6 f 6 6 . . 
+. 6 6 6 6 6 . . 
+. . . . . 6 . . 
+. . . . . 6 . . 
+`, img`
+. 8 8 8 8 8 . . 
+8 8 8 8 8 8 8 . 
+. f 6 f 6 6 . . 
+. 6 6 6 6 6 . . 
+. 6 6 f 6 6 . . 
+. 6 6 6 6 6 . . 
+. 6 . . . . . . 
+. 6 . . . . . . 
+`];
+            case "anim2":return [img`
+. 8 8 8 8 8 . . 
+8 8 8 8 8 8 8 . 
+. f 6 f 6 6 . . 
+. 6 6 6 6 6 . . 
+. 6 6 f 6 6 . . 
+. 6 6 6 6 6 . . 
+. 6 . . . 6 . . 
+. 6 . . . 6 . . 
+`];
+            case "anim3":return [img`
+. 8 8 8 8 8 . . 
+8 8 8 8 8 8 8 . 
+. 6 6 f 6 f . . 
+. 6 6 6 6 6 . . 
+. 6 6 f 6 6 . . 
+. 6 6 6 6 6 . . 
+. 6 . . . . . . 
+. 6 . . . . . . 
+`, img`
+. 8 8 8 8 8 . . 
+8 8 8 8 8 8 8 . 
+. 6 6 f 6 f . . 
+. 6 6 6 6 6 . . 
+. 6 6 f 6 6 . . 
+. 6 6 6 6 6 . . 
+. . . . . 6 . . 
+. . . . . 6 . . 
+`];
+            case "anim4":return [img`
+. 8 8 8 8 8 . . 
+8 8 8 8 8 8 8 . 
+. 6 6 f 6 f . . 
+. 6 6 6 6 6 . . 
+. 6 6 f 6 6 . . 
+. 6 6 6 6 6 . . 
+. 6 . . . 6 . . 
+. 6 . . . 6 . . 
+`];
+            case "anim5":return [img`
+. 8 8 8 8 8 . . 
+8 8 8 8 8 8 8 . 
+. 6 f 6 f 6 . . 
+. 6 6 6 6 6 . . 
+. 6 6 f 6 6 . . 
+. 6 6 6 6 6 . . 
+. 6 . . . 6 . . 
+. 6 . . . 6 . . 
+`];
+            case "anim6":return [img`
+. 8 8 8 8 8 . . 
+8 8 8 8 8 8 8 . 
+. 6 6 6 6 6 . . 
+. 6 f 6 f 6 . . 
+. 6 6 6 6 6 . . 
+. 6 6 f 6 6 . . 
+. 6 . . . 6 . . 
+. 6 . . . 6 . . 
+`];
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
